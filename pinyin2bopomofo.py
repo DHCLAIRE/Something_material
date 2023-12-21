23395eb7e393c9500386d0a4eb6f476eec161784
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2023-12-21 10:17:42 +0800

    Add open csv data commands

--- pinyin2bopomofo.py
+++ pinyin2bopomofo.py
@@ -17,7 +17,7 @@ from pathlib import Path
 #import re
 #from nltk import sent_tokenize
 #from nltk import tokenize
-from pyzhuyin import pinyin_to_zhuyin  #, zhuyin_to_pinyin
+from pyzhuyin import pinyin_to_zhuyin, zhuyin_to_pinyin
 
 
 if __name__ == "__main__":
@@ -30,4 +30,24 @@ if __name__ == "__main__":
     with open(corpus_datapath / 'corpus_FF_FB_20161206.csv', 'r', encoding = "utf-8") as corpus_csvf:
         fileLIST = corpus_csvf.read().split("\n")
         
+        for row in fileLIST[1:100]:
+            rowLIST = row.split(",")
+            print(len(rowLIST), rowLIST)
+            
+            
+            bpmfSTR = rowLIST[3]
+            toneSTR = rowLIST[4]
+            
+            
+            print(bpmfSTR, toneSTR)
+            
+            # the transformation
+            assert(pinyin_to_zhuyin("lu3") == "ㄌㄨˇ")
+            assert(pinyin_to_zhuyin("dan4") == "ㄉㄢˋ")
+            #assert(map(pinyin_to_zhuyin, ["lu3", "dan4"]) == ["ㄌㄨˇ", "ㄉㄢˋ"])
+            
+            assert(zhuyin_to_pinyin("ㄌㄩˊ") == "lü2")
+            assert(zhuyin_to_pinyin("˙ㄗ") == "zi5")
+            #assert(map(lambda z: zhuyin_to_pinyin(z, u_to_v=True), ["ㄌㄩˊ", "˙ㄗ"]) == ["lv2", "zi5"])
+            
         
