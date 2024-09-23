a905b72ac98b2201d8b3df6249ec51684b504951
DHCLAIRE <54883032+DHCLAIRE@users.noreply.github.com>
2024-09-23 20:34:44 +0800

    Create SyntaxTree and update the Python packages

---
+++ SyntaxTree.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+import sys, re
+
+from pprint import pprint
+import csv
+import json
+import random
+from random import sample
+import os
+#from gtts import gTTS
+import pandas as pd
+import time
+from pathlib import Path
+#import nltk
+#import re
+#from nltk import sent_tokenize
+#from nltk import tokenize
+from pyzhuyin import pinyin_to_zhuyin  #, zhuyin_to_pinyin
+
+
+if __name__ == "__main__":
